a698bcc126607d7647dc2f9a317732397a4d1745
satyamdev <satyam.indhane@gmail.com>
2020-12-20 22:38:01 +0530

    updated with validation and login page

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] ='3a4dc33f5c7a3b772f03800d2b3ecf98'
 
 posts = [
 
@@ -27,6 +30,29 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html',title = 'ABT')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','success')
+        return redirect(url_for('home'))
+
+    return render_template('register.html',title = 'Register', form = form)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'dog@123.com' and form.password.data == '12345':
+            flash('You have been logged in!','success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password','danger')
+
+    return render_template('login.html',title = 'Login', form = form)
+
 
 
 if __name__ == '__main__':
